158fc132656282a097becd4329a47f4bae0a1deb
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2025-12-08 09:27:18 -0400

    main branch

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+
+# file: test_calculator.py
+import unittest
+from calculator import add
+
+class TestCalculator(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-2, 3), 1)
+
+if __name__ == '__main__':
+    unittest.main()
