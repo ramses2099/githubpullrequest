edca0b4b7a6e378ef87b6139322c30a0a7ff00e5
Jose Encarnacion <Jose.Encarnacion@dpworld.com.do>
2025-12-08 10:26:17 -0400

    feature branch

--- test_calculator.py
+++ test_calculator.py
@@ -12,7 +12,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add_mixed_numbers(self):
         self.assertEqual(add(-2, 3), 1)
-    
+    # test comment
     def test_add_two_numbers(self):
         self.assertEqual(add(4, 4), 8)
 
